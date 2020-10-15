6718d9a4e272d6215602e007b12d6dbfa6744947
Eduardo Monteiro <eduardormonteiro@gmail.com>
2020-10-14 22:26:28 -0300

    integrate to backend API

--- src/pages/CreateOrphanage.tsx
+++ src/pages/CreateOrphanage.tsx
@@ -1,93 +1,187 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent } from "react";
 import { Map, Marker, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
+import {LeafletMouseEvent} from 'leaflet';
+import { useHistory } from "react-router-dom";
 
 import { FiPlus } from "react-icons/fi";
 
-import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/create-orphanage.css';
 import Sidebar from "../components/Sidebar";
-
-const happyMapIcon = L.icon({
-  iconUrl: mapMarkerImg,
-
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [0, -60]
-})
+import mapIcon from '../utils/mapIcons';
+import { useState } from "react";
+import api from "../services/api";
 
 export default function CreateOrphanage() {
+  const history = useHistory();
+  const [position, setPosition] = useState({ latitude: 0 , longitude: 0});
+  const [name, setName] = useState('');
+  const [about, setAbout] = useState('');
+  const [instructions, setInstructions] = useState('');
+  const [opening_hours, setOpening_hours] = useState('');
+  const [open_on_weekends, setOpenOnWeekends] = useState(true);
+  const [images, setImages] = useState<File[]>([]);
+  const [previewImages, setPreviewImages] = useState<string[]>([]);
 
+  function handleMapClick(event: LeafletMouseEvent){
+    const {lat, lng} = event.latlng;
+    console.log(event.latlng);
+    setPosition({
+      latitude: lat,
+      longitude: lng
+    });
+  }
+  function handleSelectImages(event: ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files){
+      return;
+    }
+    const selectedImages = Array.from(event.target.files);
+    setImages(selectedImages);
+
+    const selectedImagesPreview = selectedImages.map(image => {
+      return URL.createObjectURL(image)
+
+    })
+    setPreviewImages(selectedImagesPreview);
+  }
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const {latitude, longitude} = position;
+
+    const data = new FormData();
+    data.append('name', name);
+    data.append('about', about);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('instructions', instructions);
+    data.append('opening_hours', opening_hours);
+    data.append('open_on_weekends', String(open_on_weekends));
+    images.forEach(image => {
+      data.append('images', image)
+    })
+    await api.post('orphanages', data);
+    alert('Register success!');
+    history.push('/app');
+    console.log({
+      name,
+      about,
+      latitude,
+      longitude,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+      images
+    })
+  }
   return (
     <div id="page-create-orphanage">
       <Sidebar />
 
       <main>
-        <form className="create-orphanage-form">
+        <form onSubmit={handleSubmit} className="create-orphanage-form">
           <fieldset>
             <legend>Dados</legend>
 
             <Map 
-              center={[-27.2092052,-49.6401092]} 
+              center={[-23.4914438,-46.6224882]} 
               style={{ width: '100%', height: 280 }}
               zoom={15}
+              onClick={handleMapClick}
             >
               <TileLayer 
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
-
-              <Marker interactive={false} icon={happyMapIcon} position={[-27.2092052,-49.6401092]} />
+              { position.latitude !== 0 ?
+              <Marker 
+                interactive={false} 
+                icon={mapIcon} 
+                position={[
+                  position.latitude, 
+                  position.longitude]} />
+              : null }
             </Map>
 
             <div className="input-block">
-              <label htmlFor="name">Nome</label>
-              <input id="name" />
+              <label htmlFor="name">Name</label>
+              <input 
+                id="name" 
+                value={name} 
+                onChange={event => setName(event.target.value) } />
             </div>
 
             <div className="input-block">
-              <label htmlFor="about">Sobre <span>Máximo de 300 caracteres</span></label>
-              <textarea id="name" maxLength={300} />
+              <label htmlFor="about">About <span>300 characters maximum</span></label>
+              <textarea 
+                id="about" 
+                value={about} 
+                onChange={event => setAbout(event.target.value) } 
+                maxLength={300} />
             </div>
 
             <div className="input-block">
-              <label htmlFor="images">Fotos</label>
+              <label htmlFor="images">Pictures</label>
 
-              <div className="uploaded-image">
-
+              <div className="images-container">
+                { previewImages.map(image => {
+                  return (
+                    <img key={image} src={image} alt={name} />
+                  )
+                })
+                }
+                <label htmlFor="image[]" className="new-image">
+                  <FiPlus size={24} color="#15b6d6" />
+                </label>
+                
               </div>
-
-              <button className="new-image">
-                <FiPlus size={24} color="#15b6d6" />
-              </button>
+              <input multiple onChange={handleSelectImages} type="file" id="image[]" />
             </div>
           </fieldset>
 
           <fieldset>
-            <legend>Visitação</legend>
+            <legend>Visiting</legend>
 
             <div className="input-block">
-              <label htmlFor="instructions">Instruções</label>
-              <textarea id="instructions" />
+              <label htmlFor="instructions">Instructions</label>
+              <textarea 
+                id="instructions" 
+                value={instructions} 
+                onChange={event => setInstructions(event.target.value) }
+                />
             </div>
 
             <div className="input-block">
-              <label htmlFor="opening_hours">Nome</label>
-              <input id="opening_hours" />
+              <label htmlFor="opening_hours">Opening hours</label>
+              <input 
+                id="opening_hours" 
+                value={opening_hours} 
+                onChange={event => setOpening_hours(event.target.value) }
+                />
             </div>
 
             <div className="input-block">
-              <label htmlFor="open_on_weekends">Atende fim de semana</label>
+              <label htmlFor="open_on_weekends">Opened on weekends</label>
 
               <div className="button-select">
-                <button type="button" className="active">Sim</button>
-                <button type="button">Não</button>
+                <button 
+                  type="button" 
+                  className={open_on_weekends ? 'active' : ''}
+                  onClick= { () => setOpenOnWeekends(true)}
+                >
+                    Yes
+                </button>
+                <button 
+                  type="button"
+                  className={!open_on_weekends ? 'active' : ''}
+                  onClick= { () => setOpenOnWeekends(false)}
+                >
+                    No
+                </button>
               </div>
             </div>
           </fieldset>
 
           <button className="confirm-button" type="submit">
-            Confirmar
+            Confirm
           </button>
         </form>
       </main>
--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
-//import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css'
 import mapIcon from '../utils/mapIcons';
+import api from '../services/api';
 
+interface Orphanage {
+    id: number;
+    latitude: number;
+    longitude: number;
+    name: string;
+};
 
 
 function OrphanagesMap(){
+    const [orphanages, setOrphanages] = useState <Orphanage[]>([]);
+    //console.log(orphanages);
+    useEffect(() => {
+        api.get('orphanages').then(response => {
+            setOrphanages(response.data);
+        })
+    }, []);
     return (
         <div id="page-map">
             <aside> 
@@ -34,19 +47,25 @@ function OrphanagesMap(){
                 <TileLayer 
                 url = {`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
-                <Marker 
-                    icon = {mapIcon}
-                    position = {[-23.4958645!,-46.6222915]}
-                >
 
-                    <Popup closeButton= {false} minWidth={240} maxWidth={240} className='map-popup' >
-                            Lar das crianças
-                            <Link to="/orphanages/1">
-                                <FiArrowRight size={20} color="#FFF" />
-                            </Link>
-                    </Popup>
-                </Marker>
+        	    { orphanages.map (orphanage => {
+                    return (
+                    <Marker 
+                        icon = {mapIcon}
+                        position = {[orphanage.latitude ,orphanage.longitude ]}
+                        key = {orphanage.id}
+                    >
+    
+                        <Popup closeButton= {false} minWidth={240} maxWidth={240} className='map-popup' >
+                                {orphanage.name}
+                                <Link to= {`/orphanages/${orphanage.id}`}>
+                                    <FiArrowRight size={20} color="#FFF" />
+                                </Link>
+                        </Popup>
+                    </Marker>
+                    )
 
+                })}
             </Map>
             <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
